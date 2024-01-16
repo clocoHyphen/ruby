bf4c2fc719df3bfd61106e4dcc84382c367c2cb8
clocoHyphen <kc.tashi@cloco.co.jp>
2024-01-16 16:35:14 +0545

    ruby init

--- first.rb
+++ first.rb
@@ -1,34 +1,19 @@
-#!/usr/bin/ruby
-
-$nice = "q"
-NICE = 2
-class Customer
-    @@no_of_customers = 0
-    def initialize(id, name, addr)
-       @cust_id = id
-       @cust_name = name
-       @cust_addr = addr
-    end
-    def display_details()
-    #    puts "Customer id #@cust_id"
-    #    puts "Customer name #@cust_name"
-    #    puts "Customer address #@cust_addr"
-       puts "Global address #$nice"
-       puts "COnstant  #{NICE}"
-       puts "Multiplication Value : #{NICE}";
-    end
-    def total_no_of_customers()
-       @@no_of_customers += 1
-       puts "Total number of \e customers:" + " #@@no_of_customers"
-    end
- end
- 
- # Create Objects
- cust1 = Customer.new("1", "John", "Wisdom Apartments, Ludhiya")
- cust2 = Customer.new("2", "Poul", "New Empire road, Khandala")
- cust3 = Customer.new("3", "Poudsl", "Ndasdasew Empire road, Khandala")
- 
- # Call Methods
- cust1.total_no_of_customers()
- cust2.total_no_of_customers()
- cust3.total_no_of_customers()
+CONST = ' out there'
+class Inside_one
+   CONST = ' in there'
+   def where_is_my_CONST
+      CONST + ' inside one'
+   end
+end
+class Inside_two
+   CONST = ' inside two'
+   def where_is_my_CONST
+      CONST
+   end
+end
+puts Inside_one.new.where_is_my_CONST
+puts Inside_two.new.where_is_my_CONST
+puts Object::CONST + Inside_two::CONST
+puts Inside_two::CONST + CONST
+puts Inside_one::CONST
+puts Inside_one::CONST.call + Inside_two::CONST
